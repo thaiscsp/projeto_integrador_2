3fd9ba01e2b78ec4bf874f49b54f53afc5735060
thaiscsp <2014835@aluno.univesp.br>
2022-06-29 16:22:03 -0300

    Integração front + back end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,21 +20,7 @@
 					</div>
 				");
 		}
-	?>
-
-	<!--<form id='cadastro-newsletter' action='' method='POST'>
-		@csrf
-		<br>
-		<label for='nome'><span style='color: red;'>*</span> Nome:</label>
-		<input type='text' id='nome' name='nome' required>
-		<br><br>
-		<label for='email'><span style='color: red;'>*</span> Email:</label>
-		<input type='email' id='email' name='email' required>
-		<br><br>
-		<input type='submit' value='Cadastrar'>
-		<br>
-	</form>-->
-		
+	?>	
 
 		<!-- Quem somos -->
 		<div id="quem-somos"></div>

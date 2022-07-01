03b1456c958328285f6ccd9903462c78c3e56df5
thaiscsp <104219628+thaiscsp@users.noreply.github.com>
2022-07-01 19:41:04 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -113,7 +113,7 @@
         <!---->
         <!-- Galeria -->
         <section class="bg-dark" id="galeria">
-            <!--<div class="container">
+            <div class="container">
                 <div class="bg-dark">
                     <div class="container">
                         <h1
@@ -123,7 +123,7 @@
                         </h1>
                     </div>
                 </div>
-                <div class="row p-3 border-white">
+                <div class="row p-3 border-white container">
                             <div class="col-lg-4 col-md-4 col-sm-12 item card w-100 thumbnail">
                                 <img class="img-fluid" src="https://i.imgur.com/vA9buFK.jpg" class="card-img-top" alt="">
                             </div>
@@ -149,28 +149,7 @@
                             </div>
                         </div>-->
                 </div>
-            </div>-->
-            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="https://i.imgur.com/vA9buFK.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="https://i.imgur.com/hh9WROI.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://i.imgur.com/fTFHzey.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+            </div>
         </section>
 
 		<!-- Fale conosco -->

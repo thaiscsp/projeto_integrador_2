603e4f994204f4195873a385371bb66d5f5c87b4
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-01 19:24:16 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -352,22 +352,20 @@ if ($err) {
 				});
 			})();
 
-            $('.owl-carousel').owlCarousel({
-                loop:true,
-                margin:10,
-                nav:false,
-                dots:false,
-                autoplay: true,
-                smartSpeed: 300,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:3
-                    }
-                }
-            })
+            $('.owl-carousel');owl.owlCarousel({
+    		items:4,
+    		loop:true,
+    		margin:10,
+    		autoplay:true,
+    		autoplayTimeout:1000,
+    		autoplayHoverPause:true
+	    });
+            $('.play').on('click',function(){
+         owl.trigger('play.owl.autoplay',[1000])
+        })
+            $('.stop').on('click',function(){
+         owl.trigger('stop.owl.autoplay')
+    })
 		</script>
 
 

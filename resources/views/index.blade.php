1faed38eff46ae8c6fa424f675f4b23c274a8b2c
thaiscsp <104219628+thaiscsp@users.noreply.github.com>
2022-07-01 19:38:28 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -113,7 +113,7 @@
         <!---->
         <!-- Galeria -->
         <section class="bg-dark" id="galeria">
-            <div class="container">
+            <!--<div class="container">
                 <div class="bg-dark">
                     <div class="container">
                         <h1
@@ -149,7 +149,28 @@
                             </div>
                         </div>-->
                 </div>
-            </div>
+            </div>-->
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://i.imgur.com/vA9buFK.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://i.imgur.com/hh9WROI.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.imgur.com/fTFHzey.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
         </section>
 
 		<!-- Fale conosco -->

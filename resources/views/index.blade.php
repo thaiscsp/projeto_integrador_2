3d9883bc8c0b9768ffc9fb32be8e06014abe4821
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 13:05:52 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,3 @@
-<?php
-$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
-$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
-$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
-$symbian =  strpos($_SERVER['HTTP_USER_AGENT'],"Symbian");
-
-if ($iphone || $ipad || $android || $palmpre || $ipod || $berry || $symbian == true) {
-    echo "Olá, eu sou mobile";
-} else {
-    echo "Olá, eu sou um computador";
-}
-?>
 @extends ('layouts.master')
 
 @section('title', 'Cervejaria Dela')
@@ -371,6 +356,22 @@ if ($err) {
                     }
                 }
             })
+            
+            <?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$symbian =  strpos($_SERVER['HTTP_USER_AGENT'],"Symbian");
+
+if ($iphone || $ipad || $android || $palmpre || $ipod || $berry || $symbian == true) {
+    echo "Olá, eu sou mobile";
+} else {
+    echo "Olá, eu sou um computador";
+}
+?>
 		</script>
 
 

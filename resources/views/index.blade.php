d74f1adf619d6f812a3fd6b83abf89e68bb770f1
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 13:10:22 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,3 @@
-<?php
-$mobile = FALSE;
-
-$user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
-
-foreach($user_agents as $user_agent){
-
-    if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
-        $mobile = TRUE;
-
-        $modelo = $user_agent;
-
-        break;
-    }
-}
-
-if ($mobile){
-
-    echo "Acesso feito via ".strtolower($modelo);
-
-}else{
-
-    echo "Acesso feito via computador";
-}
-?>
-
 @extends ('layouts.master')
 
 @section('title', 'Cervejaria Dela')
@@ -385,6 +359,31 @@ if ($err) {
         
 		</script>
 
+<?php
+$mobile = FALSE;
+
+$user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
+
+foreach($user_agents as $user_agent){
+
+    if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
+        $mobile = TRUE;
+
+        $modelo = $user_agent;
+
+        break;
+    }
+}
+
+if ($mobile){
+
+    echo "Acesso feito via ".strtolower($modelo);
+
+}else{
+
+    echo "Acesso feito via computador";
+}
+?>
 
 
 

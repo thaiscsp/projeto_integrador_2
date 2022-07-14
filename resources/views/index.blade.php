1920a5748b10094dfc54a4cae88f1956f96d7097
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 13:24:43 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,3 @@
-@media (max-width: 1024px) {
-.c1025 {
- display: none;
-}
-@media (min-width: 1025px) {
-.u1025 {
- display: none;
-}
-}
-@media (max-width: 499px) {
-.c499 {
- display: none;
-}
-}
-@media (max-width: 799px) {
-.c799 {
- display: none;
-}
-}
-@media (max-width: 640px) {
-.c640 {
- display: none;
-}
-}
-@media (max-width: 360px) {
-.c360 {
- display: none;
-}
-}
-
-
 @extends ('layouts.master')
 
 @section('title', 'Cervejaria Dela')

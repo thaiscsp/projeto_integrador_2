867a486e79fd8b0928a449f488b2a002b99514b4
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 12:57:40 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@ use Jenssegers\Agent\Agent;
 
 $agent = new Agent();
 
+$agent->setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2');
+$agent->setHttpHeaders($headers);
+
 @section('title', 'Cervejaria Dela')
 
 @section('content')

43957026e21a8b84fb7e224d6f2d0b2dec388377
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 12:01:41 -0300

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -59,7 +59,7 @@
 			}
 
 			/*Nossas cervejas*/
-			@media screen and (max-width: 567px) {
+			@media screen and (max-width: 320px) {
 				.order-text {
 					order: 1;
 				}
@@ -70,7 +70,7 @@
 
 			/* Conheça nossa cervejaria */
 
-			@media screen and (max-width: 567px) {
+			@media screen and (max-width: 320px) {
 				.container {
 					text-align: center;
 				}
@@ -84,7 +84,7 @@
 			}
 
 			/*Galeria*/
-			@media screen and (min-width: 567px) {
+			@media screen and (min-width: 320px) {
 				.cards-wrapper {
 					display: flex;
 				}
@@ -97,7 +97,7 @@
 				}
 			}
 
-			@media screen and (max-width: 567px) {
+			@media screen and (max-width: 320px) {
 				.card:not(:first-child) {
 					display: none;
 				}

21603fb653a049f8ecc2d221dbb92cd93fe0cc62
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 12:51:55 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -211,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Agent' => Jenssegers\Agent\Facades\Agent::class,
     ])->toArray(),
 
 ];

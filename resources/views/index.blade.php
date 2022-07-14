24a411ba17d986df37dd54ccc1a5e98bcbb89453
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-07-14 13:17:04 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -359,115 +359,30 @@ if ($err) {
         
         
 <?php
-// Basic detection.
-$detect->isMobile();
-$detect->isTablet();
+$mobile = FALSE;
 
-// Magic methods.
-$detect->isIphone();
-$detect->isSamsung();
-// [...]
+$user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
 
-// Alternative to magic methods.
-$detect->is('iphone');
+foreach($user_agents as $user_agent){
 
-// Find the version of component.
-$detect->version('Android');
+    if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
+        $mobile = TRUE;
 
-// Additional match method.
-$detect->match('regex.*here');
+        $modelo = $user_agent;
 
-// Browser grade method.
-$detect->mobileGrade();
-
-// Batch methods.
-$detect->setUserAgent($userAgent);
-$detect->setHttpHeaders($httpHeaders);
-<?php
-// Check for mobile environment.
-if ($detect->isMobile()) {
-    // Your code here.
-}
-<?php
-// Check for tablet device.
-if($detect->isTablet()){
-    // Your code here.
-}
-<?php
-// Check for any mobile device, excluding tablets.
-if ($detect->isMobile() && !$detect->isTablet()) {
-    // Your code here.
-}
-<?php
-//  Keep the value in $_SESSION for later use
-//    and for optimizing the speed of the code.
-if(!$_SESSION['isMobile']){
-    $_SESSION['isMobile'] = $detect->isMobile();
-}
-<?php
-// Redirect the user to your mobile version of the site.
-if($detect->isMobile()){
-    header('http://m.yoursite.com', true, 301);
-}
-<?php
-// Include and instantiate the class.
-require_once 'Mobile_Detect.php';
-$detect = new Mobile_Detect;
-
-// Any mobile device (phones or tablets).
-if ( $detect->isMobile() ) {
-
+        break;
+    }
 }
 
-// Any tablet device.
-if( $detect->isTablet() ){
+if ($mobile){
 
+    echo "Acesso feito via ".strtolower($modelo);
+
+}else{
+
+    echo "Acesso feito via computador";
 }
-
-// Exclude tablets.
-if( $detect->isMobile() && !$detect->isTablet() ){
-
-}
-
-// Check for a specific platform with the help of the magic methods:
-if( $detect->isiOS() ){
-
-}
-
-if( $detect->isAndroidOS() ){
-
-}
-
-// Alternative method is() for checking specific properties.
-// WARNING: this method is in BETA, some keyword properties will change in the future.
-$detect->is('Chrome')
-$detect->is('iOS')
-$detect->is('UCBrowser')
-$detect->is('Opera')
-// [...]
-
-// Batch mode using setUserAgent():
-$userAgents = array(
-'Mozilla/5.0 (Linux; Android 4.0.4; Desire HD Build/IMM76D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19',
-'BlackBerry7100i/4.1.0 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/103',
-// [...]
-);
-foreach($userAgents as $userAgent){
-
-  $detect->setUserAgent($userAgent);
-  $isMobile = $detect->isMobile();
-  $isTablet = $detect->isTablet();
-  // Use the force however you want.
-
-}
-
-// Get the version() of components.
-// WARNING: this method is in BETA, some keyword properties will change in the future.
-$detect->version('iPad'); // 4.3 (float)
-$detect->version('iPhone') // 3.1 (float)
-$detect->version('Android'); // 2.1 (float)
-$detect->version('Opera Mini'); // 5.0 (float)
-// [...]
+?>
 
 		</script>
 
